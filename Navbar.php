07fce377363429990e0cb84ffb479a7ee44bc08b
nabilarishajuliana <nabila_juliana_30rpl@student.smktelkom-mlg.sch.id>
2025-08-29 10:25:36 +0700

    first commit

---
+++ Navbar.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+     body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+
+    .navbar {
+        background: #002366;
+        padding: 15px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        /* kiri - kanan */
+    }
+
+    .navbar-left {
+        display: flex;
+        align-items: center;
+        gap: 30px;
+    }
+
+    .navbar ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+
+    .navbar ul li {
+        margin-right: 20px;
+    }
+
+    .navbar ul li a {
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    .logout-button {
+        color: red;
+        background-color: aliceblue;
+        padding: 8px 15px;
+        border-radius: 5px;
+        text-decoration: none;
+        font-weight: bold;
+    }
+</style>
+
+<body>
+     <!-- Navbar -->
+    <div class="navbar">
+        <div class="navbar-left">
+            <img src="https://unair.ac.id/wp-content/uploads/2021/04/Logo-Universitas-Airlangga-UNAIR.png"
+                style="height:50px;">
+        </div>
+        <div class="navbar-center">
+            <ul>
+                <li><a href="/RSH/admin.php">Home</a></li>
+                <li><a href="/RSH/DataMaster.php">Data Master</a></li>
+            </ul>
+        </div>
+        <div class="navbar-right">
+            <a href="logout.php" class="logout-button">Logout</a>
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ admin.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["username"])) {
+    header( "location:login.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+
+    .navbar {
+        background: #002366;
+        padding: 15px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        /* kiri - kanan */
+    }
+
+    .navbar-left {
+        display: flex;
+        align-items: center;
+        gap: 30px;
+    }
+
+    .navbar ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+
+    .navbar ul li {
+        margin-right: 20px;
+    }
+
+    .navbar ul li a {
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    .logout-button {
+        color: red;
+        padding: 8px 15px;
+        border-radius: 5px;
+        text-decoration: none;
+        font-weight: bold;
+    }
+</style>
+
+<body>
+   <?php
+   include("Navbar.php");
+   ?>
+
+    <div class="content">
+        <h2>Hai, <?php echo $_SESSION['nama'] ?>!</h2>
+        <h3>selamat datang di halaman admin</h3>
+    </div>
+</body>
+
+</html>
